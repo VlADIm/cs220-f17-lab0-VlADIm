7fa103b83d5215e630510a1678c84900f9c3ca35
Vladimir Malcevic <vmalcev1@binghamton.edu>
2017-08-24 12:13:15 -0400

    My first commit!

--- Makefile
+++ Makefile
@@ -1,1 +1,10 @@
 # Makefile
+
+all: hw
+
+hw: hw.c
+	gcc hw.c -o hw
+
+clean:
+	rm -f hw
+	rm *~ #*#
--- hw.c
+++ hw.c
@@ -1,1 +1,8 @@
 // hw.c
+
+#include <stdio.h>
+
+int main() {
+	printf("Hello World!\n");
+	return 0;
+}
